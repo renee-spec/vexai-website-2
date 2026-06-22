cfe6b6ef9106accd42ae680645cb96e8b6feacba
renee-spec <renee@vexai.nl>
2026-06-22 02:36:33 +0200

    Add files via upload

---
+++ vexai-website-2/astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+
+export default defineConfig({
+  output: 'static',
+  site: 'https://www.vexai.nl',
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes('/404'),
+    }),
+  ],
+});
